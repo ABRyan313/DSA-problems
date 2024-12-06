4951e79570b510b50b5a72cb9aee18d7479c7c58
Md. Abir Hossain <abir4044@diu.edu.bd>
2024-12-06 20:05:40 +0600

    Duplicate Array element

---
+++ Duplicate_2/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        int [] num1 ={1,2,3,3};
+        int [] num2 ={1,2,3,4};
+
+        System.out.println(s.hasDuplicate(num1));
+        System.out.println(s.hasDuplicate(num2));
+
+
+    }
+}
