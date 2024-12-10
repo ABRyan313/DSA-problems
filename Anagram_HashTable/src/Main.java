92fbfc6bbb1a16f450ac5e427876a3b9ee6e1285
Md. Abir Hossain <abir4044@diu.edu.bd>
2024-12-10 21:23:03 +0600

    Anagram check With HashMap

---
+++ Anagram_HashTable/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String a = ("abir");
+        String b = ("rbia");
+
+        System.out.println(s.isAnagram(a,b));
+    }
+}
---
+++ Anagram_Hashmap/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        String a = ("abir");
+        String b = ("rbia");
+
+        System.out.println(s.isAnagram(a,b));
+    }
+}
